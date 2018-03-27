5354ad580d2be50312987b4d7ef89349a4d7d123
Paul Camarata <camarata.sa@wkst.pc.local>
2018-03-26 17:31:30 -0700

    major overhaul

--- client/client.go
+++ client/client.go
@@ -3,14 +3,16 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"crypto/sha1"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"time"
 )
 
+//global varables
 var (
 	JOIN_REQ    string = "JR"
 	PASS_REQ    string = "PQ"
@@ -19,20 +21,139 @@ var (
 	DATA        string = "DA"
 	TERMINATE   string = "TE"
 	REJECT      string = "RE"
-	reader             = bufio.NewReader(os.Stdin)
+	pload       string = "1111" //padding to meet spec
+	oFile       string
 )
+
+//initialization funcion
+func init() {
+	//print line numbers for debugging
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
 
 //error checking function, crash on fail
 func eC(err error) {
 	if err != nil {
-		log.Fatalln("Fatal error:", err)
+		log.Fatalln("Fatal error, ABORT:", err)
+	}
+}
+
+//function to handle cleartext password delivery
+func auth(conn net.Conn) {
+	//prompt for password
+	fmt.Print("Enter password:")
+
+	//read password from stdout
+	resp, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	//check for error
+	eC(err)
+
+	// strip newline char from resp var
+	resp = PASS_RESP + pload + resp[:len(resp)-1]
+
+	//convert password to byte array
+	buff := []byte(resp)
+
+	//send password to server
+	_, err = conn.Write(buff)
+
+	//check for error
+	eC(err)
+
+	//reurn to listening state
+	lC(conn)
+
+}
+
+//listen on connection function
+func lC(conn net.Conn) {
+
+	//create byte slice for storing payload
+	p := make([]byte, 2048)
+
+	for {
+		//get length of payload
+		n, err := conn.Read(p)
+
+		//check for error
+		eC(err)
+
+		header := string(p[0:2])
+		payload := string(p[2:6])
+		therest := string(p[6:n])
+
+		/* //Debugging code (comment out when not needed)
+		log.Println("header=", header, "payload=", payload, "therest =", therest)
+		log.Println(n)     //debug for length of byte array received
+		log.Println(p[0:n]) //debug for content of byte array received
+		*/
+
+		switch header {
+		case PASS_REQ:
+			auth(conn)
+		case PASS_ACCEPT:
+			//ready to receive data
+			lC(conn)
+		case DATA:
+			if payload == "1111" {
+
+				err := ioutil.WriteFile(oFile, p[6:n], 0644)
+				eC(err)
+			} else if payload == "1112" {
+				//create a new empty buffer
+				buf := bytes.NewBuffer(nil)
+
+				//open file on local system
+				f, err := os.Open(oFile)
+
+				//check for error
+				eC(err)
+
+				io.Copy(buf, f)
+
+				//stop working with file
+				f.Close()
+
+				//converting buffer to string for later processing
+				s := string(buf.Bytes())
+
+				//build sha1 digest
+				r := sha1.Sum([]byte(s))
+
+				//build an empty buffer
+				buff := []byte{}
+
+				//build checksum
+				for i := 1; i < sha1.Size; i++ {
+					buff = append(buff, r[i])
+				}
+				if string(buff) == therest {
+
+					log.Fatalln("Checksum verified: OK")
+				} else {
+					log.Fatalln("Checksum failed: Abort")
+				}
+			}
+
+		case REJECT:
+			log.Println("server rejected password, please try again")
+			auth(conn)
+
+		case TERMINATE:
+			log.Fatalln("ABORT")
+		default:
+			log.Println("ABORT")
+
+		}
 	}
 }
 
 //join request function
 func jR(conn net.Conn) {
-	//initialize message to server
-	buf := []byte(JOIN_REQ)
+
+	//initialize message request to server
+	buf := []byte(JOIN_REQ + pload)
 
 	//send initial message
 	_, err := conn.Write(buf)
@@ -44,72 +165,24 @@ func jR(conn net.Conn) {
 	lC(conn)
 }
 
-//listen on connection function
-func lC(conn net.Conn) {
-	//define payload
-	p := make([]byte, 2048)
-
-	for {
-		//get length of payload
-		n, err := conn.Read(p)
-
-		//check for error
-		eC(err)
-
-		//convert payload to string
-		input := string(p[:bytes.IndexByte(p, 0)])
-
-		log.Println(n)     //debug for length of byte array received
-		log.Println(input) //debug for content of byte array received
-
-		switch input {
-		case PASS_REQ:
-			auth(conn)
-		case PASS_ACCEPT:
-		case REJECT:
-			//		case "DATA":
-
-		case TERMINATE:
-		default:
-			log.Println(input)
-			err := ioutil.WriteFile("./dat1", p, 0644)
-			eC(err)
-
-		}
-
-		time.Sleep(time.Second * 1)
-	}
-}
-
-//function to handle cleartext password delivery
-func auth(conn net.Conn) {
-	//prompt for password
-	fmt.Print("Enter password:")
-
-	//read password from stdout
-	resp, err := reader.ReadString('\n')
-
-	//check for error
-	eC(err)
-
-	// strip newline char from resp var
-	resp = resp[:len(resp)-1]
-
-	//convert password to byte array
-	buff := []byte(resp)
-
-	//send password to server
-	_, err = conn.Write(buff)
-
-	//check for error
-	eC(err)
-
-	lC(conn)
-
-}
-
 func main() {
-	conn, err := net.Dial("udp", "127.0.0.1:8080")
+
+	//capture command arguments to be leveraged n connectoin creation
+	clo := os.Args
+
+	//verify correct number of arguments presented to initialize server
+	if len(clo) != 4 {
+		log.Fatal("Incorrect setup: ./client <server name> <server port> <output file>")
+	}
+
+	//capture desired output location
+	oFile = clo[3]
+
+	//server log statement
+	log.Println("Inital settings: server =", clo[1], "port =", clo[2], "output file =", clo[3])
+
+	//connect to server
+	conn, err := net.Dial("udp", clo[1]+":"+clo[2])
 	eC(err)
 
 	//if all actions complete close connection
--- server/server.go
+++ server/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha1"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strconv"
 )
 
 var (
@@ -16,84 +18,186 @@ var (
 	DATA        string = "DA"
 	TERMINATE   string = "TE"
 	REJECT      string = "RE"
-	p                  = make([]byte, 2048)
+	epload      string = "1111" //empty payload
+	dpload      string = "1112" //digest payload
+	sPass       string
+	iFile       string
 )
+
+//initialization funcion
+func init() {
+	//print line numbers for debugging
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
 
 //error checking function, crash on fail
 func eC(err error) {
 	if err != nil {
-		log.Fatalln("Fatal error:", err)
+		log.Fatalln("Fatal error, ABORT:", err)
 	}
 }
 
 //send file function
 func sF(ser *net.UDPConn, remoteaddr *net.UDPAddr) {
+	//create a new empty buffer
 	buf := bytes.NewBuffer(nil)
-	f, _ := os.Open("./somefile") // Error handling elided for brevity.
-	io.Copy(buf, f)               // Error handling elided for brevity.
-	log.Println(f.Name())
+
+	//open file on local system
+	f, err := os.Open(iFile)
+
+	//check for error
+	eC(err)
+
+	io.Copy(buf, f)
+
+	//server side logging statement
+	log.Println("Attempting to send:", f.Name())
+
+	//stop working with file
 	f.Close()
 
+	//converting buffer to string for later processing
 	s := string(buf.Bytes())
-	log.Println(s)
-	ser.WriteToUDP([]byte(DATA), remoteaddr)
 
-	ser.WriteToUDP(buf.Bytes(), remoteaddr)
+	//sending file
+	_, err = ser.WriteToUDP([]byte(DATA+epload+s), remoteaddr)
+
+	//check for error
+	eC(err)
+
+	//build sha1 digest
+	r := sha1.Sum([]byte(s))
+
+	//possibly for the future, if max payload exceeded then crash
+	//max := make([]byte, len(DATA)+len(dpload)+sha1.Size)
+
+	//build an empty buffer
+	buff := []byte{}
+
+	//append packet type to buffer
+	buff = append(buff, DATA...)
+
+	//append payload type to buffer
+	buff = append(buff, dpload...)
+
+	//build checksum
+	for i := 1; i < sha1.Size; i++ {
+		buff = append(buff, r[i])
+	}
+
+	//server side logging statement
+	log.Println("Sending checksum")
+
+	//send checksum
+	_, err = ser.WriteToUDP(buff, remoteaddr)
+
+	//
+	log.Println("Checksum sent")
+
+	//check for error
+	eC(err)
+
+	//last action; return to listen state
 	defer listenS(ser)
 }
 
 func authS(ser *net.UDPConn, remoteaddr *net.UDPAddr) {
-	ser.WriteToUDP([]byte(PASS_REQ), remoteaddr)
-	for i := 0; i <= 1; i++ {
-		_, remoteaddr, err := ser.ReadFromUDP(p)
+	ser.WriteToUDP([]byte(PASS_REQ+epload), remoteaddr)
+	l := make([]byte, 2048)
+
+	for i := 1; i < 3; i++ {
+		n, remoteaddr, err := ser.ReadFromUDP(l)
 		eC(err)
-		input := string(p[:bytes.IndexByte(p, 0)])
-		if input == "password" {
-			ser.WriteToUDP([]byte(PASS_ACCEPT), remoteaddr)
-			log.Println("Password accepted")
-			sF(ser, remoteaddr)
+
+		header := string(l[0:2])
+		password := string(l[6:n])
+
+		if header == PASS_RESP {
+			if password == sPass {
+				ser.WriteToUDP([]byte(PASS_ACCEPT+epload), remoteaddr)
+				log.Println("Password accepted", "Password attempt #", i)
+				sF(ser, remoteaddr)
+			} else {
+				log.Println("Bad Password: Attempt #", i)
+				ser.WriteToUDP([]byte(REJECT+epload), remoteaddr)
+			}
 		} else {
-			log.Println("Bad Password: Attempt", i)
-			ser.WriteToUDP([]byte(PASS_REQ), remoteaddr)
+			//handle termination unhandled message
 		}
 	}
-	ser.WriteToUDP([]byte(REJECT), remoteaddr)
+	ser.WriteToUDP([]byte(TERMINATE+epload), remoteaddr)
 	defer listenS(ser)
 }
 
 func listenS(ser *net.UDPConn) {
 	for {
+		p := make([]byte, 2048)
 		n, remoteaddr, err := ser.ReadFromUDP(p)
 		eC(err)
+		header := string(p[0:2])
 
 		input := string(p[:bytes.IndexByte(p, 0)])
+
+		//Server side logging statement
 		log.Println("rAddr:", remoteaddr, "payload =", input, "length =", n)
 
-		if bytes.HasPrefix([]byte(input), []byte(JOIN_REQ)) {
+		switch header {
+		case JOIN_REQ:
+			//handle join request
 			authS(ser, remoteaddr)
-		} else if bytes.HasPrefix([]byte(input), []byte(PASS_RESP)) {
-			// PASS_ACCEPT()
-		} else {
-			//handle rejection function
-			ser.WriteToUDP([]byte(REJECT), remoteaddr)
-			log.Println("failed: rAddr: ", remoteaddr, "payload = ", input)
+		default:
+			//close connection
+			ser.Close()
+
+			//Server side log
+			log.Println("ABORT")
 		}
 	}
 }
 
 func main() {
+	//capture command arguments to be leveraged n connectoin creation
+	clo := os.Args
 
+	//verify correct number of arguments presented to initialize server
+	if len(clo) != 4 {
+		log.Fatal("Incorrect setup: ./server <server port> <password> <input file>")
+	}
+
+	//server log statement
+	log.Println("Inital settings: port =", clo[1], "password =", clo[2], "input file =", clo[3])
+
+	//save password to be used in authentication
+	sPass = clo[2]
+
+	//save file to be used in sendFile
+	iFile = clo[3]
+
+	//convert "port" string value to int
+	port, err := strconv.Atoi(clo[1])
+
+	//check for error
+	eC(err)
+
+	//define server lsten bind parameters
 	addr := net.UDPAddr{
-		Port: 8080,
+		Port: port,
 		IP:   net.ParseIP("127.0.0.1"),
 	}
 
+	//server initialize log statement
 	log.Println("Starting server on", addr)
+
+	//start listener
 	ser, err := net.ListenUDP("udp", &addr)
+
+	//check for error
 	eC(err)
 
+	//listener handler
 	defer listenS(ser)
 
+	//server started log statement
 	log.Println("Server started successfully")
 
 }
